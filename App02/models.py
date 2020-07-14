1022f842f4a16fa4b03e65e29db5f3030603ea21
D.venkata Rao <venkatarao.dharamna59@gmail.com>
2020-07-14 16:03:22 +0530

    continuing validation

--- App02/models.py
+++ App02/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Student_register(models.Model):
+    regno  = models.AutoField(primary_key = True)
+    Name = models.CharField(max_length=50)
+    contact = models.IntegerField(unique=True)
+    email = models.EmailField(max_length=50,unique=True)
+    user = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
